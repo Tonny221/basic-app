8d6d84a6b7986d973241f960f27f0ed22ca20307
Tonny221 <anthonny14gabriell@gmail.com>
2021-08-23 14:42:49 -0400

    basic interaction

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import { useState } from 'react';
+
+import Hello from './components/hello';
+
 function App() {
+  const [ showMassage, setShowMassage] = useState(false);
+
+  function buttonHandler(){
+    setShowMassage(true);
+  }
+
   return (
-    <div></div>
+    <div>
+      <button className='btn' onClick={buttonHandler}>Click Here!</button>
+      { showMassage && <Hello />}
+    </div>
   );
 }
 
